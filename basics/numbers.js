4f143c398f25766d47907530e25cfb44ad36d53f
ValentinCaudana <valentincaudana19@gmail.com>
2025-08-15 11:32:37 +0200

    Variables with numbers

---
+++ basics/numbers.js
@@ -0,0 +1,12 @@
+
+//studentScore 18
+let studentScore = 18
+
+//maxScore
+let maxScore = 20
+
+// percent 90
+let percent = (studentScore / maxScore) * 100
+
+// print the percent
+console.log(percent)
---
+++ basics/strings.js
@@ -0,0 +1,5 @@
+let city = 'Cordoba';
+let country = 'Argentina';
+let location = city + ','+ country;
+
+console.log(location);
