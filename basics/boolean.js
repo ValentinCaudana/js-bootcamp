b1761a13b9ca796573d5409adea981429a6510cc
ValentinCaudana <valentincaudana19@gmail.com>
2025-08-18 09:24:09 +0200

    Booleans and comparison operator

---
+++ basics/boolean.js
@@ -0,0 +1,18 @@
+// === - equalitu operator
+// !== - no equal operator
+// < - less than operator
+// > - greater than operator
+// <= - less than or equal to operator
+// >= - greater than or equal to operator
+
+
+//create age set to your age
+let age = 26
+// calculate is child - if they are 7 or under 
+let isChild = age <= 7
+// calculate is senior - if they are 65 or older
+let isSenior = age >= 65
+// print is child value
+console.log(isChild)
+//print is senior value
+console.log(isSenior)
