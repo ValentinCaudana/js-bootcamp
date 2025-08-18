fa40f2873f1433ed33c2a858af503b5d0514684e
ValentinCaudana <valentincaudana19@gmail.com>
2025-08-18 09:48:22 +0200

    Comparasion

--- basics/boolean.js
+++ basics/boolean.js
@@ -7,7 +7,7 @@
 
 
 //create age set to your age
-let age = 26
+let age = 6
 // calculate is child - if they are 7 or under 
 let isChild = age <= 7
 // calculate is senior - if they are 65 or older
@@ -16,3 +16,13 @@ let isSenior = age >= 65
 console.log(isChild)
 //print is senior value
 console.log(isSenior)
+
+//COMPARASION
+//If 7 or under print message about child pricing 
+if (age <= 7) {
+  console.log('You have 10% of discount!')
+}
+// if 65 or older print message about senior discount
+if (age >= 65) {
+  console.log('You have 30% of discount!')
+}
