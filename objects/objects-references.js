262fd8958dbb9457c6b652ae5cfcd192e1b9af8c
ValentinCaudana <valentincaudana19@gmail.com>
2025-08-20 18:48:23 +0200

    challenge of objects

--- objects/objects-references.js
+++ objects/objects-references.js
@@ -7,14 +7,40 @@ let myAccount = {
 
 let addExpense = function (account, amount) {
     account.expenses = account.expenses + amount
-    console.log(account)
-    
-/*  Whith this both logs print differen things
-    account = {}
-    account.age = 1
-    console.log(account)
-*/    
+}
+// addIncome
+
+let addIncome = function (account, income) {
+    account.income = account.income + income 
 }
 
-addExpense(myAccount, 2000)
-console.log(myAccount)
+
+// resetAccount
+let resetAccount = function (account) {
+    account.expenses = 0
+    account.income = 0
+}
+
+// getAccountSumary
+//Account for ... has $900. $1000 in income. $100 in expenses.
+
+let getAccountSumary = function (account) {
+    let balance = account.income - account.expenses
+    return `Account for ${account.name} has $${balance}. $${account.income} in income. $${account.expenses} in expenses.`
+}
+
+addIncome(myAccount, 2000)
+addExpense(myAccount, 300)
+addExpense(myAccount, 2.5)
+console.log(getAccountSumary(myAccount))
+resetAccount(myAccount)
+console.log(getAccountSumary(myAccount))
+
+
+//addIncome
+//add Expenses
+//addExpenses
+//getAccountSumary
+//resetAccount
+//getAccountSumary
+
