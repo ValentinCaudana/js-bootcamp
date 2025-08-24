3f25e5abefc16512b92fd812a11aacec127f692f
ValentinCaudana <valentincaudana19@gmail.com>
2025-08-24 11:14:52 +0200

    indexOf and findIndex

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,4 +1,13 @@
-const notes = [`Note 1`, `Note 2`, `Note 3`]
+const notes = [{}, {
+    title: 'My next trip',
+    body: 'I would like to go to Spain'
+}, {
+    title: 'Habbits to work on',
+    body: 'Exercise. Eating a bit better.'
+}, {
+    title: 'Office modification',
+    body: 'Get a new seat'
+}]
 
 //console.log(notes) // bring all the note
 //console.log(notes.length) // number of how many items we have
@@ -14,15 +23,15 @@ const notes = [`Note 1`, `Note 2`, `Note 3`]
 //notes.splice(1, 1, `This is the new second item`)
 //("first we say where we start", "we say what item we want to delete", "we add an item") we can delete an item and also add an item or one off thems
 
-notes[2] = `This is now the new note 3` //we replace de item that we call
+//notes[2] = `This is now the new note 3` //we replace de item that we call
 
 /*.forEach our callback function gets called one time for each in the array.
 The callback function gets called with the individual array item and the index for that item
-*/
+
 notes.forEach(function (item, index){
     console.log(index)// bring the number in the index (this case=0)
     console.log(item)// bring the item exaqli the same that in the array
-})
+})*/
 
 console.log(notes.length)
 console.log(notes)
@@ -40,3 +49,9 @@ for (let count = 0; count <= 2; count++) {
 for (let count = notes.length - 1; count >= 0; count--) {
  console.log(notes[count])
 }*/
+
+//console.log(notes.indexOf({}))// bring the position of the item that we call (1)
+const index = notes.findIndex(function (note, index){// allow us to provide our own definition of what makes two things equal 
+    return note.title === `Habbits to work on`// vulue title return true from the callback function when you've found the match
+})
+console.log(index)
