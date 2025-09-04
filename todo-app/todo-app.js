5b59cd65cd1ad0eea142fb64fa1ea6a9dc18971e
ValentinCaudana <valentincaudana19@gmail.com>
2025-09-04 11:56:17 +0200

    Refactor Challenge

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -3,7 +3,7 @@
 // 3. Stringify and write the data when new data is added 
 
 
-let todos = []
+const todos = getSavedTodos()
 
 /*
 const ps = document.querySelectorAll('p') //we select what value we want to finde "p"
@@ -17,47 +17,7 @@ const filters = {
     searchText: '',
     hideCompleted: false
 }
-const todoJSON = localStorage.getItem('todos')
 
-if (todoJSON !== null){
-    todos = JSON.parse(todoJSON)
-}
-
-
-const renderTodos = function (todos, filters) { 
-    let filteredTodos= todos.filter(function (todo) {
-        // theerd solution 
-        const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
-        const hideCompleted = !filters.hideCompleted || !todo.completed
-        return searchTextMatch && hideCompleted 
-    })
-//seconde solution 
-    //filteredTodos = filteredTodos.filter(function (todo) {
-      // return !filters.hideCompleted || !todo.completed
-/* first solution 
-        if(filters.hideCompleted){
-            return !todo.completed
-        }else {
-            return true
-        }*/
-    //})
-
-    const incompleteTodos = filteredTodos.filter (function (todos){ // we creat the .filter to see the thing that we need in the array-object
-        return !todos.completed 
-    })
-
-    document.querySelector('#todos').innerHTML = ''
-
-    const sumary = document.createElement('h2') // we create the new value
-    sumary.textContent = `You have ${incompleteTodos.length} todos left.`
-    document.querySelector('#todos').appendChild(sumary)
-    
-    filteredTodos.forEach (function (todo){ // first we call the function with .forEach to see all the array-object
-        const p = document.createElement('p') 
-        p.textContent = todo.text
-        document.querySelector('#todos').appendChild(p)
-    })
-}
 
 renderTodos(todos, filters)
 
@@ -72,22 +32,12 @@ document.querySelector('#todo-form').addEventListener('submit', function (e){
         text: e.target.elements.insertTodo.value,
         completed: false
     })
-/* my form
-    const newTodoText = e.target.elements.insertTodo.value.trim()
-    if (newTodoText.length > 0) {
-        todos.push({
-            text: newTodoText,
-            completed: false
-        })*/
-        localStorage.setItem('todos', JSON.stringify(todos)) // we use that to change the object in a string
+        saveTodos(todos)
         renderTodos(todos, filters)
-        e.target.elements.insertTodo.value = ''
-    
+        e.target.elements.insertTodo.value = ''  
 })
-// 1. Create a checkbox and set up event listener -> "Hide completed"
-// 2- Create new hideCompleted filter (default false)
-// 3. Update hideCompleted an rerender list on checkbox change
-// 4. Set up renderTodos to remove completed items
+
+
 document.querySelector('#check-todos').addEventListener('change', function (e){
     filters.hideCompleted = e.target.checked
     renderTodos(todos, filters) 
---
+++ todo-app/todo-functions.js
@@ -0,0 +1,53 @@
+// 1.
+const getSavedTodos = function () {
+    const todoJSON = localStorage.getItem('todos')
+
+    if (todoJSON !== null){
+        return  JSON.parse(todoJSON)
+    } else {
+        return []
+    } 
+}
+
+// 2.
+const saveTodos = function (todos){
+    localStorage.setItem('todos', JSON.stringify(todos)) // we use that to change the object in a string
+}
+
+
+// 3. 
+const renderTodos = function (todos, filters) { 
+    let filteredTodos= todos.filter(function (todo) {
+        const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+        const hideCompleted = !filters.hideCompleted || !todo.completed
+        return searchTextMatch && hideCompleted 
+    })
+
+    const incompleteTodos = filteredTodos.filter (function (todos){ // we creat the .filter to see the thing that we need in the array-object
+        return !todos.completed 
+    })
+
+    document.querySelector('#todos').innerHTML = ''
+
+    document.querySelector('#todos').appendChild(generateSumaryDom(incompleteTodos))
+    
+    filteredTodos.forEach (function (todo){ // first we call the function with .forEach to see all the array-object
+    
+        document.querySelector('#todos').appendChild(generateTodoDOM(todo))
+    })
+}
+
+
+// 4.
+const generateTodoDOM = function (todo){
+    const p = document.createElement('p') 
+    p.textContent = todo.text
+    return p
+}
+
+//5. 
+const generateSumaryDom = function (incompleteTodos) {
+    const sumary = document.createElement('h2') // we create the new value
+    sumary.textContent = `You have ${incompleteTodos.length} todos left.`
+    return sumary 
+}
