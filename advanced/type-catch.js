d6680d1bc83491ba91d1eb506246188dfd7a477e
ValentinCaudana <valentincaudana19@gmail.com>
2025-09-17 10:40:16 +0200

    Catching and Trowing Errors

---
+++ advanced/type-catch.js
@@ -0,0 +1,14 @@
+const getTip = (amount) => {
+    if (typeof amount === 'number') {
+        return amount * .25
+    } else {
+        throw Error('Argument must be a number') // we get this if the value in getTip() is not a number "getTip(string)" 
+    }
+}
+
+try {
+ const result = getTip(10)
+ console.log(result) // This will print if the code is correct "getTip(number)"
+} catch (e) {
+ console.log('Error!') // This will print if there are an error in the code "getTip(string)"
+}
--- functions/grade-calc.js
+++ functions/grade-calc.js
@@ -3,37 +3,32 @@
 // A 90-100, B 80-89, C 70-79, D 60-69, F 0-59
 
 const gradeCalc = function (score, totalScore){ //se mantiene a lo largo del script
-    const percent = (score / totalScore) * 100  //siempre se aplica de la misma forma a lo largo del script
-    let letterGrade = ``// aqui no podemos poner const porque en cada if le asignamos que cambie
-
-//    if (percent >= 90) {
-//       return `You got a A (${percent}%)`
-//    } else if (percent <= 89 && percent >= 80) {
-//        return `You got a B (${percent}%)`
-//    } else if (percent <= 79 && percent >= 70) {
-//        return `You got a C (${percent}%)`
-//    } else if (percent <= 69 && percent >= 60) {
-//        return `You got a D (${percent}%)`
-//    } else if (percent <=59) {
-//        return `You got a F (${percent}%)`
-//    }
-
-    if (percent >= 90){
-        letterGrade = `A`
-    } else if (percent >= 80){
-        letterGrade = `B`
-    } else if (percent >= 70){
-        letterGrade = `C`
-    } else if (percent >= 60){
-        letterGrade = `D`
-    } else {
-        letterGrade = `F`
+    if (typeof score !== 'number' || typeof totalScore !== 'number') {
+            throw Error('Please provide an number only')
     }
+            const percent = (score / totalScore) * 100  //siempre se aplica de la misma forma a lo largo del script
+        let letterGrade = ``// aqui no podemos poner const porque en cada if le asignamos que cambie
+
+        if (percent >= 90){
+            letterGrade = `A`
+        } else if (percent >= 80){
+            letterGrade = `B`
+        } else if (percent >= 70){
+            letterGrade = `C`
+        } else if (percent >= 60){
+            letterGrade = `D`
+        } else {
+            letterGrade = `F`
+        } 
     
-    return `You got ${letterGrade} (${percent}%)`
+        return `You got ${letterGrade} (${percent}%)`
+    
 
 }
 
-const  scoreText = gradeCalc(10, 20) // aqui siempre se mantiene igual ya que despues de esto no le pedimos que cambie nada
-
-console.log(scoreText)
+try {
+    const  scoreText = gradeCalc(10, true) 
+    console.log(scoreText)
+}catch (e) {
+    console.log(e.message) // whit this we call the message inside the Error('')
+}
