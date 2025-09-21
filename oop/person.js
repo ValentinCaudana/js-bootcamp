4df1248d1cff059218404f2dd31a798efc8583b8
ValentinCaudana <valentincaudana19@gmail.com>
2025-09-21 09:54:03 +0200

    Prototypical-Inheritance

--- oop/person.js
+++ oop/person.js
@@ -23,8 +23,13 @@ Person.prototype.setName = function (fullName) {
 
 
 const me = new Person('Valentin', 'Caudana', 26, ['Learning', 'Cooking']) // This statement creates me and assigns it the specified values for its properties
+
+me.getBio = function () { // this change the function getBio but just when we call me, because is linked at "me.getBio" 
+    return 'It is fake!'
+}
+
 me.setName('Alexis Turner')
-console.log(me.getBio())
+console.log(me.getBio()) // Here bring me the last string that we set in the last me.getBio function
 
 const person2 = new Person('Clancey', 'Turner', 51)
-console.log(person2.getBio())
+console.log(person2.getBio()) // Here just call the getBio function above because we didn't change the function with "person2"
