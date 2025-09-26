6f1f4020994647c2ca8828bc4c34fc09060d394f
ValentinCaudana <valentincaudana19@gmail.com>
2025-09-26 07:56:53 +0200

    HTTP Headers and Erorrs

--- hangman/app.js
+++ hangman/app.js
@@ -20,12 +20,14 @@ window.addEventListener('keypress',  (e) => {
 const request = new XMLHttpRequest()
 
 request.addEventListener('readystatechange', (e) => {
-    if (e.target.readyState === 4) {
+    if (e.target.readyState === 4 && e.target.status === 200) {
         const data = JSON.parse(e.target.responseText)
         console.log(data) // Will print a new random puzzle
- }
+    }else if (e.target.readyState === 4) {
+        console.log('An error has taken place')
+    }
 })
 
-request.open('GET', 'https://puzzle.mead.io/puzzle') // first we call the method and second we call the URL
+request.open('GET', 'https://puzzle.mead.io/puzzle?wordCount=3') // first we call the method and second we call the URL
 request.send()
 
