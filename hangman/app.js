32344e5d922039d09eda3e05fb7bc3329d550952
ValentinCaudana <valentincaudana19@gmail.com>
2025-10-13 10:59:42 +0200

    Callback-Abstraction-Challenge

--- hangman/app.js
+++ hangman/app.js
@@ -16,43 +16,20 @@ window.addEventListener('keypress',  (e) => {
 })
 
 
-getPuzzle((error, puzzle) => {
+getPuzzle("2", (error, puzzle) => {
     if (error) {
         console.log(`Error: ${error}`)
     } else {
         console.log(puzzle)
     }
 })
-// Here we cannot use a return because the getPuzzle its an eventListener and we have to use the patron "callback"
 
-/* Making an HTTP request
-const request = new XMLHttpRequest()
-
-request.addEventListener('readystatechange', (e) => {
-    if (e.target.readyState === 4 && e.target.status === 200) {
-        const data = JSON.parse(e.target.responseText)
-        console.log(data) // Will print a new random puzzle
-    }else if (e.target.readyState === 4) {
-        console.log('An error has taken place')
-    }
-})
-
-request.open('GET', 'https://puzzle.mead.io/puzzle?wordCount=3') // first we call the method and second we call the URL
-request.send()
-
-
-const countryCode = 'AR'
-const countryRequest = new XMLHttpRequest()
-
-countryRequest.addEventListener('readystatechange', (e) => {
-    if (e.target.readyState === 4 && e.target.status === 200) {
-        const data = JSON.parse(e.target.responseText)
-        const country = data.find((country) => country.cca2 === countryCode)
-        console.log(country.name.common)
+getCountry('AR', function (error, country) {
+    if (error) {
+        console.log('Error:', error)
+    } else {
+        console.log(`Country name: ${country.name.common}`) 
     }
 })
 
 
-countryRequest.open('GET', 'https://restcountries.com/v3.1/all?fields=cca2,name')
-countryRequest.send()
-*/
--- hangman/requests.js
+++ hangman/requests.js
@@ -1,4 +1,4 @@
-const getPuzzle = (callback) => {
+const getPuzzle = (wordCount, callback) => {
     const request = new XMLHttpRequest()
 
     request.addEventListener('readystatechange', (e) => {
@@ -9,6 +9,26 @@ const getPuzzle = (callback) => {
             callback('An error has taken place', undefined)
         }
     })
-    request.open('GET', 'https://puzzle.mead.io/puzzle?wordCount=3')
+    request.open('GET', `https://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
     request.send()
+    }
+
+
+
+const getCountry = (countryCode, callback) => {
+    const countryRequest = new XMLHttpRequest()    
+
+    countryRequest.addEventListener('readystatechange', (e) => {
+    if (e.target.readyState === 4 && e.target.status === 200) {
+        const data = JSON.parse(e.target.responseText)
+        const country = data.find((country) => country.cca2 === countryCode)
+        callback(undefined, country)
+    }else if (e.target.readyState === 4) {
+        console.log('We cannot get the country', undefined)
+    }
+})
+    countryRequest.open('GET', 'https://restcountries.com/v3.1/all?fields=cca2,name')
+    countryRequest.send()
+
 }
+
