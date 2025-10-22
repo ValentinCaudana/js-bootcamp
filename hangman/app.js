f9592692b56a497f511d8009ff86072a1b9888d3
ValentinCaudana <valentincaudana19@gmail.com>
2025-10-22 10:55:25 +0200

    converting-promises

--- hangman/app.js
+++ hangman/app.js
@@ -16,20 +16,17 @@ window.addEventListener('keypress',  (e) => {
 })
 
 
-getPuzzle("2", (error, puzzle) => {
-    if (error) {
-        console.log(`Error: ${error}`)
-    } else {
-        console.log(puzzle)
-    }
+getPuzzle('2').then((puzzle) => {
+    console.log(puzzle)
+}, (err) => {
+    console.log(`Error:${err}`)
 })
 
-getCountry('AR', function (error, country) {
-    if (error) {
-        console.log('Error:', error)
-    } else {
-        console.log(`Country name: ${country.name.common}`) 
-    }
-})
 
+
+getCountry('MX').then((country) => {
+    console.log(country.name.common)
+}, (err) => {
+    console.log(`Error:${err}`)
+})
 
