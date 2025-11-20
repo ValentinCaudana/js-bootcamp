fcafcb38fccd458d51220f65fb2f204061ad8e19
ValentinCaudana <valentincaudana19@gmail.com>
2025-11-20 12:10:10 +0100

    Promise-Challenge

--- hangman/app.js
+++ hangman/app.js
@@ -1,43 +1,34 @@
 // HTTP (Hypertext Transfer Protocol)
 // Request - What do we want to do
 // Response - What was actually done
-const puzzleEl = document.querySelector('#puzzle')
-const guessEl = document.querySelector('#guesses')
-const game1 = new Hangman('Car parts', 2)
-puzzleEl.textContent = game1.puzzle
-guessEl.textContent = game1.statusMessage
+const puzzleEl = document.querySelector("#puzzle");
+const guessEl = document.querySelector("#guesses");
+const game1 = new Hangman("Car parts", 2);
+puzzleEl.textContent = game1.puzzle;
+guessEl.textContent = game1.statusMessage;
 
-window.addEventListener('keypress',  (e) => {
-    const guess = String.fromCharCode(e.charCode)
-    game1.makeGuess(guess)
-    puzzleEl.textContent = game1.puzzle
-    guessEl.textContent = game1.statusMessage
-    
-})
+window.addEventListener("keypress", (e) => {
+  const guess = String.fromCharCode(e.charCode);
+  game1.makeGuess(guess);
+  puzzleEl.textContent = game1.puzzle;
+  guessEl.textContent = game1.statusMessage;
+});
 
+getPuzzle("2")
+  .then((puzzle) => {
+    console.log(puzzle);
+  })
+  .catch((err) => {
+    console.log(`Error:${err}`);
+  });
 
-getPuzzle('2').then((puzzle) => {
-    console.log(puzzle)
-}).catch((err) => {
-    console.log(`Error:${err}`)
-})
-
-
-/*
-getCountry('MX').then((country) => {
-    console.log(country.name.common)
-}, (err) => {
-    console.log(`Error:${err}`)
-})
-
-fetch('https://puzzle.mead.io/puzzle', {}).then((response) => {
-    if (response.status === 200) {
-        return response.json()
-    } else {
-        throw new Error('Unable to fetch the puzzle')
-    }
-}).then((data) => {
-    console.log(data.puzzle)
-}).catch((error)=> {
-    console.log(error)
-})*/
+getLocation()
+  .then((location) => {
+    return getCountry(location.country);
+  })
+  .then((country) => {
+    console.log(country.name.common);
+  })
+  .catch((err) => {
+    console.log(`Error:${err}`);
+  });
--- hangman/requests.js
+++ hangman/requests.js
@@ -1,29 +1,39 @@
 const getPuzzle = (wordCount) => {
-    return fetch(`https://puzzle.mead.io/puzzle?wordCount=${wordCount}`).then((response) => {
-        if(response.status === 200) {
-            return response.json()
-        } else {
-            throw new Error('Unable to fetch puzzle')
-        }
-    }).then((data) => {
-        return data.puzzle
+  return fetch(`https://puzzle.mead.io/puzzle?wordCount=${wordCount}`)
+    .then((response) => {
+      if (response.status === 200) {
+        return response.json();
+      } else {
+        throw new Error("Unable to fetch puzzle");
+      }
     })
-}
+    .then((data) => {
+      return data.puzzle;
+    });
+};
 
+const getCountry = (countryCode) => {
+  return fetch("https://restcountries.com/v3.1/all?fields=cca2,name")
+    .then((response) => {
+      if (response.status === 200) {
+        return response.json();
+      } else {
+        throw new Error("Unable to fetch data");
+      }
+    })
+    .then((data) => {
+      return data.find((country) => country.cca2 === countryCode);
+    });
+};
 
-
-const getCountry = (countryCode) => new Promise ((resolve, reject) => {
-    const countryRequest = new XMLHttpRequest()    
-
-    countryRequest.addEventListener('readystatechange', (e) => {
-    if (e.target.readyState === 4 && e.target.status === 200) {
-        const data = JSON.parse(e.target.responseText)
-        const country = data.find((country) => country.cca2 === countryCode)
-        resolve(country)
-    }else if (e.target.readyState === 4) {
-        reject('We cannot get the country')
+const getLocation = () => {
+  return fetch("https://ipinfo.io/json?token=6d0c7866a5894c").then(
+    (response) => {
+      if (response.status === 200) {
+        return response.json();
+      } else {
+        throw new Error("Unable to fetch location");
+      }
     }
-})
-    countryRequest.open('GET', 'https://restcountries.com/v3.1/all?fields=cca2,name')
-    countryRequest.send()
-})
+  );
+};
