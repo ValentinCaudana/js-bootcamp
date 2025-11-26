371b683b11c8c7941b84816f3da20cde23caf217
ValentinCaudana <valentincaudana19@gmail.com>
2025-11-26 11:12:06 +0100

    CSS-NoteApp

--- notes-app/scripts/notes-app.js
+++ notes-app/scripts/notes-app.js
@@ -1,48 +1,42 @@
-'use strict'
+"use strict";
 
-
-let notes = getSavedNotes()
-
+let notes = getSavedNotes();
 
 const filters = {
-    searchText: '',
-    sortBy: 'byEdited'
-}
+  searchText: "",
+  sortBy: "byEdited",
+};
 
-renderNotes(notes, filters) // we call the function to see the the thing before the user interact with the page 
+renderNotes(notes, filters);
 
-document.querySelector('#create-note').addEventListener('click', (e) => { // we add a button and we asg a function // (1.how we call"a click", 2. what we want to answer)
-    const id = uuidv4()
-    const timestamp = moment().valueOf()
-    notes.push ({
-        id: id,
-        title: '',
-        body: '',
-        createdAt : timestamp,
-        updatedAt: timestamp
-    })    
-    saveNotes(notes)
-    location.assign(`/edit.html#${id}`)//assignation a location to go
-})
+document.querySelector('#create-note').addEventListener("click", (e) => {
+  const id = uuidv4();
+  const timestamp = moment().valueOf();
 
+  notes.push({
+    id: id,
+    title: '',
+    body: '',
+    createdAt: timestamp,
+    updatedAt: timestamp,
+  });
+  saveNotes(notes);
+  location.assign(`/edit.html#${id}`);
+});
 
-document.querySelector('#search-text').addEventListener('input', (e) =>{ //"change" we call when we touch in another places in the window // "input we call every single change"
-        filters.searchText = e.target.value
-        renderNotes(notes, filters)
+document.querySelector("#search-text").addEventListener("input", (e) => {
+  filters.searchText = e.target.value;
+  renderNotes(notes, filters);
+});
 
-})
+document.querySelector("#filter-by").addEventListener("change", (e) => {
+  filters.sortBy = e.target.value;
+  renderNotes(notes, filters);
+});
 
-
-document.querySelector('#filter-by').addEventListener('change', (e) =>{
-    filters.sortBy = e.target.value
-    renderNotes(notes, filters)
-})
-
-window.addEventListener('storage', (e) => {
-    if (e.key === 'notes') {
-        notes = JSON.parse(e.newValue)
-        renderNotes(notes, filters)
-    }
-})
-
-
+window.addEventListener("storage", (e) => {
+  if (e.key === "notes") {
+    notes = JSON.parse(e.newValue);
+    renderNotes(notes, filters);
+  }
+});
--- notes-app/scripts/notes-functions.js
+++ notes-app/scripts/notes-functions.js
@@ -1,111 +1,120 @@
-'use strict'
+"use strict";
 
-// Read existing notes from localStorage 
+// Read existing notes from localStorage
 const getSavedNotes = () => {
-    const notesJSON = localStorage.getItem('notes')
+  const notesJSON = localStorage.getItem("notes");
 
-    try {
-        return notesJSON? JSON.parse(notesJSON) : []
-    }catch (e) {
-        return []
-    }
-
-    
-}
+  try {
+    return notesJSON ? JSON.parse(notesJSON) : [];
+  } catch (e) {
+    return [];
+  }
+};
 
 // Save the notes to localStorage
 const saveNotes = (notes) => {
-    localStorage.setItem('notes', JSON.stringify(notes)) // we use that to save the object in a string
-}
+  localStorage.setItem("notes", JSON.stringify(notes));
+};
 
-// Remove a note from the List
+// Remove a note from the list
 const removeNote = (id) => {
-    const noteIndex = notes.findIndex((note) => note.id === id)
-    if (noteIndex > -1) { // if the noteIndex is false ...
-        notes.splice(noteIndex, 1)// argument noteIndex and we want to remove one item
-    }
-}
+  const noteIndex = notes.findIndex((note) => note.id === id);
 
-// Generate the DOM structure for a note 
-const generateNotesDOM = (note) => {
-    const noteEl = document.createElement('div')
-    const textEl = document.createElement('a')
-    const button = document.createElement('button')
-        
-    //Set up remove note button
-    button.textContent = 'x'
-    noteEl.appendChild(button)
-    button.addEventListener('click', () => {
-        removeNote(note.id)
-        saveNotes(notes)
-        renderNotes(notes, filters)
-    })
+  if (noteIndex > -1) {
+    notes.splice(noteIndex, 1);
+  }
+};
 
-    // Set the note title text
-    if(note.title.length > 0){
-        textEl.textContent = note.title
+// Generate the DOM structure for a note
+const generateNoteDOM = (note) => {
+    const noteEl = document.createElement('a');
+    const textEl = document.createElement('p');
+    const statusEL = document.createElement('p')
+
+    // Setup the note title text
+    if (note.title.length > 0) {
+        textEl.textContent = note.title;
     } else {
-        textEl.textContent = 'Unnamed note'
+        textEl.textContent = "Unnamed note";
     }
+    textEl.classList.add('list-item__title')
+    noteEl.appendChild(textEl);
+
+    // Setup the link
+    noteEl.setAttribute("href", `/edit.html#${note.id}`);
+    noteEl.classList.add('list-item')
+
+    // Setup the status message
+    statusEL.textContent = generateLastEdited(note.updatedAt)
+    statusEL.classList.add('list-item__subtitle')
+    noteEl.appendChild(statusEL)
     
-    textEl.setAttribute('href', `/edit.html#${note.id}`)
-    noteEl.appendChild(textEl)
-    
-    return noteEl
-}
-// Sort Notes by one of three ways
+    return noteEl;
+};
+
+// Sort your notes by one of three ways
 const sortNotes = (notes, sortBy) => {
-    if ( sortBy === 'byEdited'){
-        return notes.sort((a, b) => {
-            if (a.updatedAt > b.updatedAt){
-                return -1
-            } else if (a.updatedAt < b.updatedAt) {
-                return 1
-            } else {
-                return 0 
-            }
-        })
-    } else if (sortBy === 'byCreation') {
-        return notes.sort((a, b) => {
-            if (a.createdAt > b.createdAt){
-                return -1
-            } else if (a.createdAt < b.createdAt){
-                return 1
-            } else {
-                return 0
-            }
-        })
-    } else if (sortBy === 'alphabetically'){
-        return notes.sort((a, b) => {
-            if (a.title.toLowerCase() < b.title.toLowerCase()){
-                return -1
-            } else if (a.title.toLowerCase() > b.title.toLowerCase()){
-                return 1
-            } else {
-                return 0
-            }
-        })
+  if (sortBy === "byEdited") {
+    return notes.sort((a, b) => {
+      if (a.updatedAt > b.updatedAt) {
+        return -1;
+      } else if (a.updatedAt < b.updatedAt) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  } else if (sortBy === "byCreated") {
+    return notes.sort((a, b) => {
+      if (a.createdAt > b.createdAt) {
+        return -1;
+      } else if (a.createdAt < b.createdAt) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  } else if (sortBy === "alphabetical") {
+    return notes.sort((a, b) => {
+      if (a.title.toLowerCase() < b.title.toLowerCase()) {
+        return -1;
+      } else if (a.title.toLowerCase() > b.title.toLowerCase()) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+  } else {
+    return notes;
+  }
+};
+
+// Render application notes
+const renderNotes = (notes, filters) => {
+    const notesEl = document.querySelector("#notes"); 
+    notes = sortNotes(notes, filters.sortBy);
+    const filteredNotes = notes.filter((note) =>
+        note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    );
+
+    notesEl.innerHTML = '';
+
+    if(filteredNotes.length > 0) {
+        filteredNotes.forEach((note) => {
+          const noteEl = generateNoteDOM(note);
+          notesEl.appendChild(noteEl)
+        });
+    }else {
+        const emptyMessage = document.createElement('p')
+        emptyMessage.textContent = 'No note to show'
+        emptyMessage.classList.add('empty-message')
+        notesEl.appendChild(emptyMessage)
     }
-    else {
-        return notes
-    }
-}
 
-// Render application notes 
-
-const renderNotes = (notes, filters) => { // take all the notes and filters and calculate which one matches with the filters
-    notes = sortNotes(notes, filters.sortBy)
-    const filteredNotes= notes.filter((note) => note.title.toLowerCase().includes(filters.searchText.toLowerCase()))// if the filter is "Nex" it was one of the titles above
-
-
-    document.querySelector('#notes').innerHTML ='' //innerHTML in this remove the others because is not the same that the filter (note)
-
-    filteredNotes.forEach((note) => { // we add the notes already filters, result in this case = My next trip
-        const noteEl = generateNotesDOM(note)       
-        document.querySelector('#notes').appendChild(noteEl)
-    })
     
-}
+};
 
 // Generate the last edited message
-const generateLastEdited = (timestamp) => `Last edited ${moment(timestamp).fromNow()}`
+const generateLastEdited = (timestamp) => {
+    return `Last edited ${moment(timestamp).fromNow()}`;
+};
