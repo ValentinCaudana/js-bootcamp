24f34332bab41166762e5601bf6104dcf819600c
ValentinCaudana <valentincaudana19@gmail.com>
2026-01-30 17:44:33 +0100

    add webpack to todo-app

---
+++ todo-app/src/index.js
@@ -0,0 +1,37 @@
+import { createTodo, loadTodos } from "./todos";
+import { setFilters } from "./filters";
+import { renderTodos } from "./views";
+
+renderTodos()
+
+document.querySelector("#search-text").addEventListener("input", (e) => {
+    setFilters({
+        searchText : e.target.value})
+    renderTodos();
+});
+
+document.querySelector("#new-todo").addEventListener("submit", (e) => {
+    const text = e.target.elements.text.value.trim();
+    e.preventDefault();
+
+    if (text.length > 0) {
+        createTodo(text)
+        renderTodos();
+        e.target.elements.text.value = "";
+    }
+});
+
+document.querySelector('#check-todos').addEventListener('change', (e) => {
+    setFilters({
+        hideCompleted : e.target.checked})
+    renderTodos()
+}) 
+
+
+window.addEventListener("storage", (e) => {
+    if (e.key === "todos") {
+        loadTodos()
+        renderTodos();
+    }
+});
+
---
+++ todo-app/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+
+module.exports = {
+  entry: ['babel-polyfill', "./src/index.js"],
+  output: {
+    path: path.resolve(__dirname, "public/scripts"), // __dirname : \Users\Lenovo\desktop\code\js-bootcamp\boilerplate/public/scripts
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_nodules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["env"],
+          },
+        },
+      },
+    ],
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "public"),
+    publicPath: "/scripts/",
+  },
+  devtool: "source-map",
+};
+
+
